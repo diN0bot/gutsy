fca0261a64ccc561b68efe355ac6bde068181321
Lucy Mendel <lucy@cloudkick.com>
2012-02-23 23:50:30 -0800

    Add middleware to '/' route that adds missing optional fields to devops object. This fixes view tests for non-full fixtures, since jade template raises excpetion for unknown fields.

--- tests/view_tests/test_index.js
+++ tests/view_tests/test_index.js
@@ -2,12 +2,16 @@ var fs = require('fs');
 var path = require('path');
 var jade = require('jade');
 var assert = require('assert');
+var middleware = require('../../lib/web/middleware');
 
+/**
+ * Tests that fixtures can be rendered by index jade template
+ */
 exports.run = function() {
   var viewTester = new ViewTester();
-  console.log("Running tests: ");
+  console.log("Running index view tests: ");
 
-  viewTester.test_view('index.jade', 'example-minimum.json');
+  viewTester.test_view('index.jade', 'example-minimum.json', [middleware.contexter]);
   viewTester.test_view('index.jade', 'example-simple.json');
   viewTester.test_view('index.jade', 'example-full.json');
 
@@ -29,15 +33,23 @@ var ViewTester = function(options) {
  *
  * @param {string} view the filename of the view relative to this._view_path
  * @param {string} devopsjson the filename of the devopsjson file relative to this._fixture_path
+ * @param {array} middlewares (optional) array of middleware functions
  * @param {function} fn (optional) a callback that takes the rendered html response as an argument
  */
-ViewTester.prototype.test_view = function(view, devopsjson, fn) {
-  var view_path, context;
+ViewTester.prototype.test_view = function(view, devopsjson, middlewares, fn) {
+    var view_path, context, middleware, i;
 
   view_path = path.join(this._view_path, view);
   context = path.join(this._fixture_path, devopsjson);
   context = fs.readFileSync(context);
   context = JSON.parse(context);
+
+  if (middlewares) {
+    for (var i = 0; i < middlewares.length; i++) {
+	middleware = middlewares[i];
+	middleware(context)(null, null, function(){});
+    }
+  }
 
   jade.renderFile(view_path, context, function(er, html) {
     assert.ifError(er);

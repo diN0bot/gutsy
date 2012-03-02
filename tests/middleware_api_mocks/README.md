ef273933d1a1584410f44c3deabd09eff1988694
lucy <lucy.mendel@rackspace.com>
2012-03-01 20:52:27 -0800

    refactor view and unit tests to reuse middleware api mocks. add more tests and enable more logic to be tested. still wish jscoverage integration worked so i could see the coverage numbers go up :-/

---
+++ tests/middleware_api_mocks/README.md
@@ -0,0 +1,11 @@
+Many middlewares make calls to external APIs. They do this via the request_maker in lib/utils.js.
+
+In order to unit test these middlewares, we mock the request maker so that the on_success and
+on_error callbacks are given hardcoded data and errors.
+
+This hardcoded data is expressed in the files in this folder.
+
+Each mock is actually a factory that returns a request maker. The factory takes two parameters:
+
+ * @param {boolean} xhr_error if True, call the on_error callback rather than on_success
+ * @param {boolean} data_error if True and if on_success is called, provide errorful data
--- tests/view_tests/test_index.js
+++ tests/view_tests/test_index.js
@@ -1,31 +1,67 @@
-var ViewTester = require('./base').ViewTester;
+var base = require('./base');
+var mocks = require('../middleware_api_mocks');
 
-exports.test_example_minimum = function(test, assert) {
-  var viewTester = new ViewTester(test, assert);
-  viewTester.test_view('index.jade', 'example-minimum.json', 'pagerduty');
+exports.test_example_minimum_none = function(test, assert) {
+  base.test_view(test, assert, 'index.jade', 'example-minimum.json', []);
 };
 
-exports.test_example_simple = function(test, assert) {
-  var viewTester = new ViewTester(test, assert);
-  viewTester.test_view('index.jade', 'example-simple.json', 'pagerduty');
+exports.test_example_minimum_pagerduty = function(test, assert) {
+  base.test_view(test, assert, 'index.jade', 'example-minimum.json', [mocks.pagerduty]);
 };
 
-exports.test_example_full = function(test, assert) {
-  var viewTester = new ViewTester(test, assert);
-  viewTester.test_view('index.jade', 'example-full.json', 'pagerduty');
+exports.test_example_minimum_versionone = function(test, assert) {
+  base.test_view(test, assert, 'index.jade', 'example-minimum.json', [mocks.versionone]);
 };
 
-/** Factory for mocking utils.request_maker */
-ViewTester.prototype._mock_maker = function(xhr_success, data_success) {
-  return function(options, on_success, on_err) {
-    if (xhr_success) {
-      if (data_success) {
-        on_success("{'entries': [{'user': {'name': 'Zaphod'}, 'start': 'starttime', 'end': 'endtime'}]}");
-      } else {
-        on_success("{'error': {'message': 'API returned error', 'code': 22}}");
-      }
-    } else {
-      on_err("mocked XHR on_err called");
-    }
-  };
+exports.test_example_minimum_all = function(test, assert) {
+  base.test_view(test, assert, 'index.jade', 'example-minimum.json', [mocks.pagerduty, mocks.versionone]);
 };
+
+exports.test_example_minimum_all_xhr_err = function(test, assert) {
+  base.test_view(test, assert, 'index.jade', 'example-minimum.json', [mocks.pagerduty, mocks.versionone], true);
+};
+
+exports.test_example_minimum_all_xhr_data_err = function(test, assert) {
+  base.test_view(test, assert, 'index.jade', 'example-minimum.json', [mocks.pagerduty, mocks.versionone], false, true);
+};
+
+exports.test_example_simple_none = function(test, assert) {
+  base.test_view(test, assert, 'index.jade', 'example-simple.json', []);
+};
+
+exports.test_example_simple_pagerduty = function(test, assert) {
+  base.test_view(test, assert, 'index.jade', 'example-simple.json', [mocks.pagerduty]);
+};
+
+exports.test_example_simple_versionone = function(test, assert) {
+  base.test_view(test, assert, 'index.jade', 'example-simple.json', [mocks.versionone]);
+};
+
+exports.test_example_simple_all = function(test, assert) {
+  base.test_view(test, assert, 'index.jade', 'example-simple.json', [mocks.pagerduty, mocks.versionone]);
+};
+
+exports.test_example_full_none = function(test, assert) {
+  base.test_view(test, assert, 'index.jade', 'example-full.json', []);
+};
+
+exports.test_example_full_pagerduty = function(test, assert) {
+  base.test_view(test, assert, 'index.jade', 'example-full.json', [mocks.pagerduty]);
+};
+
+exports.test_example_full_versionone = function(test, assert) {
+  base.test_view(test, assert, 'index.jade', 'example-full.json', [mocks.versionone]);
+};
+
+exports.test_example_full_all = function(test, assert) {
+  base.test_view(test, assert, 'index.jade', 'example-full.json', [mocks.pagerduty, mocks.versionone]);
+};
+
+exports.test_example_full_all_xhr_err = function(test, assert) {
+  base.test_view(test, assert, 'index.jade', 'example-full.json', [mocks.pagerduty, mocks.versionone], true);
+};
+
+exports.test_example_full_all_xhr_data_err = function(test, assert) {
+  base.test_view(test, assert, 'index.jade', 'example-full.json', [mocks.pagerduty, mocks.versionone], false, true);
+};
+

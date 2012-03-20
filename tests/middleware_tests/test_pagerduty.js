e662b61ad2da1d87997af5333f4d0c9361a45a91
Matt Kaniaris <mkaniaris@gmail.com>
2012-03-19 17:34:27 -0700

    time line thing is now up again.  Changed the stats so that closed tickets don't contribute to staleness and moved some var declarations.

--- tests/middleware_tests/test_pagerduty.js
+++ tests/middleware_tests/test_pagerduty.js
@@ -8,6 +8,6 @@ exports.test_example_simple = function(test, assert) {
   base.run_test(test, assert, 'example-simple.json', 'pagerduty', 'pagerduty', false);
 };
 
-exports.test_example_full__expected_timeout = function(test, assert) {
+exports.test_example_full__expected_timeout = function(test, assert){
   base.run_test(test, assert, 'example-full.json', 'pagerduty', 'pagerduty', true);
 };

f1aa09cf1bbf5e7641b2d38b2a920d9b16986bbb
Geoff Greer <geoff@greer.fm>
2012-04-03 11:09:53 -0700

    Add a tentative example of the data we're going to be pushing to gutsy.

---
+++ docs/example_data_push.js
@@ -0,0 +1,41 @@
+{
+    "time": 12345678900,
+    "name": "blah",
+    "ip": "123.56.77.6",
+    "health": 1,
+    "services": {
+        "apply_bcs-1": {
+            "service": "apply_bcs",
+            "health": 2,
+            "metrics": [
+            {
+                "type": "int",
+                "name": "bytes",
+                "value": 23456
+            },
+            {
+                "type": "int",
+                "name": "blah",
+                "value": 45678
+            }
+            ]
+        },
+        "apply_bcs-2": {
+            "service": "apply_bcs",
+            "health": 2,
+            "metrics": [
+            {
+                "type": "int",
+                "name": "bytes",
+                "value": 23456
+            },
+            {
+                "type": "int",
+                "name": "blah",
+                "value": 98732984
+            }
+            ]
+        }
+    }
+}
+};

246b4bcb54494bc1c4716e6690909eff135287e3
Geoff Greer <geoff@greer.fm>
2012-04-18 11:58:16 -0700

    Change all tabs to spaces.

--- tests/api_test.js
+++ tests/api_test.js
@@ -6,21 +6,21 @@ var _ = require('underscore');
 
 var sample_flush_payload = {"reach":{"asdf":{"services":{"a":{"health":0,"service":"asdf","timestamp":1333651513}},"timestamp":1333651513,"health":1,"name":"asdf"},"badfb78b":{"services":{"asdfasdfasdfa":{"health":0,"service":"asdf","timestamp":1333753217175}},"timestamp":1333753217175,"health":2,"name":"badfb78b"}}};
 var sample_post_payload = {"timestamp":1333753217175,"health":2,"name":"badfb78b", "services":
-	{"asdfasdfasdfa":{"health":0,"service":"asdf"}}};
+  {"asdfasdfasdfa":{"health":0,"service":"asdf"}}};
 
 exports.test_api = function(test, assert){
-	var api_cache, data;
-	var file_path = path.join(__dirname, '../lib/', settings.testing_flush_file_path);
-	try{
-		fs.unlinkSync(file_path);
-	} catch(e){}
-	fs.writeFileSync(file_path, JSON.stringify(sample_flush_payload));
-	api_cache = new utils.api_cache();
-	assert.isDefined(api_cache._projects["reach"]);
-	api_cache.flush_data(function(){
-		data = JSON.parse(fs.readFileSync(file_path));
-		assert.ok(_.isEqual(data,sample_flush_payload));
-		api_cache.handle_push("reach", sample_post_payload);
-		test.finish();
-	});
+  var api_cache, data;
+  var file_path = path.join(__dirname, '../lib/', settings.testing_flush_file_path);
+  try{
+    fs.unlinkSync(file_path);
+  } catch(e){}
+  fs.writeFileSync(file_path, JSON.stringify(sample_flush_payload));
+  api_cache = new utils.api_cache();
+  assert.isDefined(api_cache._projects["reach"]);
+  api_cache.flush_data(function(){
+    data = JSON.parse(fs.readFileSync(file_path));
+    assert.ok(_.isEqual(data,sample_flush_payload));
+    api_cache.handle_push("reach", sample_post_payload);
+    test.finish();
+  });
 };
